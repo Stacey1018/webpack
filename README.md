7dc93fb055929e6f99afc7e0bd27e1532104e953
zhangyang <zhangyang@inke.cn>
2022-03-10 11:07:24 +0800

    管理输出模块

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 ## 遇到的问题
 style-loader webpack4 需要的版本要低
+
+
+### 版本记录
+a82ee8226d263b51dd24113bb793bd6cb582c03b  管理资源部分代码
+
+
+### plugin
+
+## HtmlWebpackPlugin
+
+我们更改了入口起点的名称，或者添加了新的入口，构建时会重新命名生成的bundle,但我们的index.html 文件仍用的就的名称，htmlWebpackPlugin就可以解决这个问题
+
+版本问题：
+  npm i --save-dev html-webpack-plugin@4
+
+## CleanWebpackPlugin 
+
+清理dist目录
+
+
+// TODO
+## mainfest 
+
+webpack 通过 manifest，可以追踪所有模块到输出 bundle 之间的映射。
+
+
--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 import _ from 'lodash';
-import './style.css'
-
-import Logo from './logo.jpg'
+import printMe from './print';
 
 function component() {
     let element = document.createElement('div');
+    var btn = document.createElement('button')
   
     // lodash（目前通过一个 script 引入）对于执行这一行是必需的
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
 
-    var myLogo = new Image();
-    myLogo.src = Logo
+    btn.innerHTML = '点击这里，然后查看console'
 
-    element.appendChild(myLogo);
+    btn.onclick = printMe
+
+    element.appendChild(btn)
 
     return element;
   }
